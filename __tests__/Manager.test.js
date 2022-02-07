aa450abacdad6d8701afffbe7589362aca872e16
Nate-Huerd <nhuerd3@gmail.com>
2022-02-06 19:36:09 -0600

    adjusted HTML headers

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,11 @@
 const Manager = require('../lib/Manager');
 
 test('creates Manager object', () => {
-    const manager = new Manager('KimManager', 101, 'kim.manager@email.com', Suite101);
+    const manager = new Manager('KimManager', 101, 'kim.manager@email.com', 501);
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
 
 test('gets role', () => {
-    const manager = new Manager('KimManager', 101, 'kim.manager@email.com', Suite101);
+    const manager = new Manager('KimManager', 101, 'kim.manager@email.com', 501);
     expect(manager.officeNumber).toEqual(expect.any(Number));
 });
