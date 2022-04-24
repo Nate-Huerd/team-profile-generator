9df89cd904bc242706066bc9ff21d3a390e3aa4b
Nate-Huerd <nhuerd3@gmail.com>
2022-04-23 20:02:48 -0500

    added test code

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,11 +1,20 @@
-const Manager = require('../lib/Manager');
+const Manager = require("../lib/Manager");
+const Employee = require("../lib/Employee");
 
-test('creates Manager object', () => {
-    const manager = new Manager('KimManager', 101, 'kim.manager@email.com', 501);
-    expect(manager.officeNumber).toEqual(expect.any(Number));
+test("Can set office number via constructor argument", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.officeNumber).toBe(testValue);
 });
 
-test('gets role', () => {
-    const manager = new Manager('KimManager', 101, 'kim.manager@email.com', 501);
-    expect(manager.officeNumber).toEqual(expect.any(Number));
+test("getRole() should return \"Manager\"", () => {
+  const testValue = "Manager";
+  const e = new Manager("Foo", 1, "test@test.com", 100);
+  expect(e.getRole()).toBe(testValue);
+});
+
+test("Can get office number via getOffice()", () => {
+  const testValue = 100;
+  const e = new Manager("Foo", 1, "test@test.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
 });
